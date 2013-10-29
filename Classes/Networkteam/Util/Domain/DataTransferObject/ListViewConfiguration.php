82839b2027ca85c69f4dfb34e3c900e1c7aee890
Dominik Piekarski <dominik.piekarski@networkteam.com>
2013-10-29 15:05:44 +0100

    [FEATURE] Added SortLinkViewHelper from mueggenburg-offers for easy link sorting functionality
    Change-Id: I66d2e9044977124ba5aed774b32a88d833e8235c

---
+++ Classes/Networkteam/Util/Domain/DataTransferObject/ListViewConfiguration.php
@@ -0,0 +1,167 @@
+<?php
+namespace Networkteam\Util\Domain\DataTransferObject;
+/***************************************************************
+ *  (c) 2013 networkteam GmbH - all rights reserved
+ ***************************************************************/
+
+class ListViewConfiguration {
+
+	/**
+	 * @var string
+	 */
+	protected $sortDirection = 'ASC';
+
+	/**
+	 * @var string
+	 */
+	protected $sortProperty;
+
+	/**
+	 * array(
+	 *   'name' => array(
+	 *     'operator' => 'equals',
+	 *     'operand' => 'Foo'
+	 *   ),
+	 *   'lastUpdate' => array(
+	 *     'operator' => 'greaterThan',
+	 *     'operand' => '134234423'
+	 *   )
+	 * )
+	 *
+	 * @var array
+	 */
+	protected $filter = array();
+
+	/**
+	 * defines the properties which can be filtered
+	 * used to build the filter form
+	 * @var array
+	 */
+	protected $filterableProperties = array();
+
+	/**
+	 * Set the filter configuration
+	 *
+	 * @param array $filter
+	 */
+	public function setFilter($filter) {
+		$this->filter = $filter;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getFilter() {
+		return $this->filter;
+	}
+
+	/**
+	 * @param string $sortDirection
+	 */
+	public function setSortDirection($sortDirection) {
+		if (in_array($sortDirection, array('ASC', 'DESC'))) {
+			$this->sortDirection = $sortDirection;
+		} else {
+			$this->sortDirection = 'ASC';
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getSortDirection() {
+		return $this->sortDirection;
+	}
+
+	/**
+	 * @param string $sortField
+	 */
+	public function setSortProperty($sortField) {
+		$this->sortProperty = $sortField;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getSortProperty() {
+		return $this->sortProperty;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function hasSorting() {
+		return (string)$this->sortProperty !== '';
+	}
+
+	/**
+	 * @param array $filterableProperties
+	 */
+	public function setFilterableProperties($filterableProperties) {
+		$this->filterableProperties = $filterableProperties;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getFilterableProperties() {
+		return $this->filterableProperties;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getConfiguredFilters() {
+		$configuredFilters = array();
+		foreach ($this->filterableProperties as $name => $configuration) {
+			$filter = array(
+				'name' => $name,
+				'operand' => '',
+				'operator' => 'like'
+			);
+			if (isset($this->filter[$name])) {
+				$filter['operand'] = $this->filter[$name]['operand'];
+				$filter['operator'] = $this->filter[$name]['operator'];
+			}
+			$configuredFilters[] = $filter;
+		}
+
+		return $configuredFilters;
+	}
+
+	/**
+	 * @param string $name
+	 * @param array $configuration
+	 * @return void
+	 */
+	public function addFilter($name, $configuration) {
+		$this->filter[$name] = $configuration;
+	}
+
+	/**
+	 * This method will convert underscores in filter properties to dots for property paths
+	 * to use the property names in repositories.
+	 *
+	 * @return array
+	 */
+	public function getFilterWithPropertyPaths() {
+		$filter = $this->filter;
+		foreach ($filter as $propertyName => $propertyValue) {
+			if (strpos($propertyName, '_') !== FALSE) {
+				unset($filter[$propertyName]);
+				$propertyName = strtr($propertyName, '_', '.');
+				$filter[$propertyName] = $propertyValue;
+			}
+		}
+		return $filter;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getSortPropertyWithPropertyPath() {
+		return strtr($this->sortProperty, '_', '.');
+	}
+}
+
+?>
---
+++ Classes/Networkteam/Util/Domain/Repository/AbstractFilterableRepository.php
@@ -0,0 +1,98 @@
+<?php
+namespace Networkteam\Util\Domain\Repository;
+
+/***************************************************************
+ *  (c) 2013 networkteam GmbH - all rights reserved
+ ***************************************************************/
+
+use Networkteam\Util\Domain\DataTransferObject\ListViewConfiguration;
+use TYPO3\Flow\Persistence\QueryInterface;
+
+abstract class AbstractFilterableRepository extends \TYPO3\Flow\Persistence\Doctrine\Repository {
+
+	/**
+	 * @var array
+	 */
+	protected $filterableProperties = array();
+
+	/**
+	 * @param \Networkteam\Util\Domain\DataTransferObject\ListViewConfiguration $listViewConfiguration
+	 * @return \TYPO3\Flow\Persistence\QueryResultInterface
+	 */
+	public function findByListViewConfiguration(ListViewConfiguration $listViewConfiguration) {
+		$query = $this->createQuery();
+
+		$filter = $listViewConfiguration->getFilterWithPropertyPaths();
+
+		$constraint = $this->buildFiltersConstraint($filter, $query);
+		if ($constraint !== NULL) {
+			$query->matching($constraint);
+		}
+
+		if ($listViewConfiguration->hasSorting()) {
+			$query->setOrderings(array($listViewConfiguration->getSortPropertyWithPropertyPath() => $listViewConfiguration->getSortDirection()));
+		}
+
+		return $query->execute();
+	}
+
+	/**
+	 * @param array $filters
+	 * @param \TYPO3\Flow\Persistence\QueryInterface $query
+	 * @return object A constraint for the filters or NULL if no filters apply
+	 */
+	protected function buildFiltersConstraint($filters, $query) {
+		$constraints = array();
+		foreach ($filters as $propertyName => $filter) {
+			$constraint = $this->getConstraintForFilter($filter, $propertyName, $query);
+			if ($constraint !== NULL) {
+				$constraints[] = $constraint;
+			}
+		}
+		if (count($constraints) > 0) {
+			return $query->logicalAnd($constraints);
+		} else {
+			return NULL;
+		}
+	}
+
+	/**
+	 * @param array $filter
+	 * @param string $propertyName
+	 * @param \TYPO3\Flow\Persistence\QueryInterface $query
+	 * @return object
+	 * @throws \InvalidArgumentException
+	 */
+	protected function getConstraintForFilter(array $filter, $propertyName, QueryInterface $query) {
+		$constraint = NULL;
+		if (!isset($this->filterableProperties[$propertyName])) {
+			throw new \InvalidArgumentException('Filter for property "' . $propertyName . '" not supported', 1369912305);
+		}
+		if (!isset($filter['operator'])) {
+			throw new \InvalidArgumentException('No operator for property "' . $propertyName . '"', 1369993785);
+		}
+		switch ($filter['operator']) {
+			case 'equals':
+				if ((string)$filter['operand'] !== '') {
+					$constraint = $query->equals($propertyName, $filter['operand']);
+				}
+				break;
+			case 'like':
+				if ((string)$filter['operand'] !== '') {
+					$constraint = $query->like($propertyName, '%' . $filter['operand'] . '%');
+				}
+				break;
+			case 'is':
+				if ((string)$filter['operand'] === 'NULL') {
+					$constraint = $query->equals($propertyName, NULL);
+				}
+				break;
+			default:
+				throw new \InvalidArgumentException('Unknown operator "' . $filter['operator'] . '"', 1369911739);
+		}
+
+		return $constraint;
+	}
+
+}
+?>
