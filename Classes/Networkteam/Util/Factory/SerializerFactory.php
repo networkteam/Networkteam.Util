badf5a6b984cad53c834af71464ac346c3f4ae2b
Christian Lange <christian.lange@networkteam.com>
2017-04-25 12:14:45 +0200

    TASK: Apply migration TYPO3.Flow-20161115140430
    Adjust to the renaming of the Object namespace in Flow 4.0

--- Classes/Networkteam/Util/Factory/SerializerFactory.php
+++ Classes/Networkteam/Util/Factory/SerializerFactory.php
@@ -21,7 +21,7 @@ class SerializerFactory {
 	protected $serializer;
 
 	/**
-	 * @var \TYPO3\Flow\Object\ObjectManagerInterface
+	 * @var \TYPO3\Flow\ObjectManagement\ObjectManagerInterface
 	 * @Flow\Inject
 	 */
 	protected $objectManager;
