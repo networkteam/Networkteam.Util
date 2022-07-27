07f43e10075668ea000c79ede03e38d32ee17222
Nils Jung <nils.jung@networkteam.com>
2022-07-27 17:50:34 +0200

    add php constraint

--- Tests/Unit/Mail/MailMessage.php
+++ Tests/Unit/Mail/MailMessage.php
@@ -10,15 +10,15 @@ use Networkteam\Util\Mail\MailHeader;
 class MailMessage implements \Networkteam\Util\Mail\MailerMessageInterface
 {
 
-    /**
-     * @var array
-     */
-    protected $recipients;
+	/**
+	 * @var array
+	 */
+	protected $recipients;
 
-    /**
-     * @var string
-     */
-    protected $replyTo;
+	/**
+	 * @var string
+	 */
+	protected $replyTo;
 
 
 	/**
@@ -26,86 +26,86 @@ class MailMessage implements \Networkteam\Util\Mail\MailerMessageInterface
 	 */
 	protected array $headers;
 
-    /**
-     * Returns the mimetype of the message(text/html)
-     *
-     * @return string
-     */
-    public function getFormat()
-    {
-        // TODO: Implement getFormat() method.
-    }
+	/**
+	 * Returns the mimetype of the message(text/html)
+	 *
+	 * @return string
+	 */
+	public function getFormat()
+	{
+		// TODO: Implement getFormat() method.
+	}
 
-    /**
-     * Returns the message body, can be either HTML or text
-     *
-     * @return string
-     */
-    public function getBody()
-    {
-        // TODO: Implement getBody() method.
-    }
+	/**
+	 * Returns the message body, can be either HTML or text
+	 *
+	 * @return string
+	 */
+	public function getBody()
+	{
+		// TODO: Implement getBody() method.
+	}
 
-    /**
-     * @return mixed
-     */
-    public function getFrom()
-    {
-        // TODO: Implement getFrom() method.
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getFrom()
+	{
+		// TODO: Implement getFrom() method.
+	}
 
-    /**
-     * @return array An array of recipient addresses (string or array)
-     */
-    public function getRecipient()
-    {
-        return $this->recipients;
-    }
+	/**
+	 * @return array An array of recipient addresses (string or array)
+	 */
+	public function getRecipient()
+	{
+		return $this->recipients;
+	}
 
-    /**
-     * @return string
-     */
-    public function getSubject()
-    {
-        // TODO: Implement getSubject() method.
-    }
+	/**
+	 * @return string
+	 */
+	public function getSubject()
+	{
+		// TODO: Implement getSubject() method.
+	}
 
-    /**
-     * @return array<MailHeader>
-     */
-    public function getHeaders() : array
-    {
+	/**
+	 * @return array<MailHeader>
+	 */
+	public function getHeaders() : array
+	{
 		return $this->headers;
-    }
+	}
 
-    /**
-     * @return string A recipient identifier (not necessarily an email address) for logging
-     */
-    public function getRecipientIdentifier()
-    {
-        // TODO: Implement getRecipientIdentifier() method.
-    }
+	/**
+	 * @return string A recipient identifier (not necessarily an email address) for logging
+	 */
+	public function getRecipientIdentifier()
+	{
+		// TODO: Implement getRecipientIdentifier() method.
+	}
 
-    public function getReplyTo()
-    {
-        return $this->replyTo;
-    }
+	public function getReplyTo()
+	{
+		return $this->replyTo;
+	}
 
-    /**
-     * @param mixed $recipients
-     */
-    public function setRecipients($recipients)
-    {
-        $this->recipients = $recipients;
-    }
+	/**
+	 * @param mixed $recipients
+	 */
+	public function setRecipients($recipients)
+	{
+		$this->recipients = $recipients;
+	}
 
-    /**
-     * @param string $replyTo
-     */
-    public function setReplyTo($replyTo)
-    {
-        $this->replyTo = $replyTo;
-    }
+	/**
+	 * @param string $replyTo
+	 */
+	public function setReplyTo($replyTo)
+	{
+		$this->replyTo = $replyTo;
+	}
 
 	/**
 	 * @param array<string> $headers
